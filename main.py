67cea9a794e8b6a166c3d8afce3709679299bfc2
piotrwozn <piotrwozn@gmail.com>
2024-11-05 21:24:30 +0100

    kolejna próba

--- main.py
+++ main.py
@@ -48,4 +48,4 @@ def predict():
     return jsonify({'predictions': output})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
